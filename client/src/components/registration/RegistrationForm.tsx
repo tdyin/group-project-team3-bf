93d30e888349ab8d4fbfbb842d2c45bd34502397
John Nguyen <aznxmist@gmail.com>
2022-11-22 15:19:03 -0600

    Added initial axios data

--- client/src/components/registration/RegistrationForm.tsx
+++ client/src/components/registration/RegistrationForm.tsx
@@ -1,37 +1,85 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import useMultiForm from './useMultiForm';
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import Address from './Address';
 import NameInfo from './NameInfo';
 import User from './User';
-
-type FormData = {
-    username: string,
-    password: string,
-    confirmPass: string,
-    email: string
-}
-const defaultData: FormData = {
-    username: "",
-    password: "",
-    confirmPass: "",
-    email: ""
-}
+import axios from 'axios';
 
 const RegistrationForm = () => {
-    const [data, setData] = useState(defaultData)
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [matchPass, setMatchPass] = useState("");
 
-    const { steps, currentStep, step, isFirst, isLast, backStep, nextStep } = useMultiForm([<User />, <NameInfo />, <Address />]);
+    const userRef: any = useRef();
+    const emailRef: any = useRef();
 
-    const onSubmit = (e: FormEvent) => {
+    //const { steps, currentStep, step, isFirst, isLast, backStep, nextStep } = useMultiForm([<User />, <NameInfo />, <Address />]);
+
+    const onSubmit = async (e: FormEvent) => {
         e.preventDefault();
-        nextStep();
+        //nextStep();
+
+        try {
+            await axios.post('', JSON.stringify({username, password, email}))
+        } catch (err: any) {
+            
+        }
     }
+
+
     //TODO: Update Styles
-
     return (
-
+        <>
+            <form onSubmit={onSubmit}>
+                <TextField
+                    label="E-mail Address"
+                    size="small"
+                    variant="standard"
+                    type="text"
+                    id="email"
+                    ref={emailRef}
+                    autoComplete="off"
+                    onChange={ (e) => {setEmail(e.target.value)}}
+                    required
+                    />
+                <TextField
+                    label="Username"
+                    size="small"
+                    variant="standard"
+                    type="text"
+                    id="username"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={ (e) => {setUsername(e.target.value)}}
+                    required
+                    />
+                
+                <TextField
+                    label="Password"
+                    size="small"
+                    variant="standard"
+                    type="password"
+                    id="password"
+                    onChange={ (e) => {setPassword(e.target.value)}}
+                    required
+                    />
+                <TextField
+                    label="Confirm Password"
+                    size="small"
+                    variant="standard"
+                    type="password"
+                    id="confirmPass"
+                    onChange={ (e) => {setMatchPass(e.target.value)}}
+                    required 
+                    />
+                
+                <Button disabled={!username || !email || !password || !matchPass ? true : false}>Register</Button>
+            </form>
+        </>
     )
 
     /*

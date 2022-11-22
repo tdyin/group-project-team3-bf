f8701931d6ed8d898e8a00b3d67fdc0f899ce42d
Jinyan Li <jinyanli@DESKTOP-G24AK6R.localdomain>
2022-11-22 18:02:15 -0500

    login feature first commit

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import User from '../models/User';
 
+
 //User Registration
 export const post_register = async(req : Request, res: Response) => {
     try {
@@ -34,3 +35,34 @@ export const post_register = async(req : Request, res: Response) => {
         res.status(409).send(err);
     }
 }
+
+export const post_login = async(req: Request, res: Response) => {
+    try{
+        const hashPass = await bcrypt.hash(req.body.password, 10);
+
+        const user = await User.find({username: req.body.username, password: hashPass});
+
+        if (user.length === 0) {
+            res.status(404).send('Invalid username/password');
+        } else {
+            // req.session.username = req.body.username;
+            // req.session.user_id = user.id;
+            // req.session.isLoggedIn = true;
+            res.status(201).send("Account was successfully login.");
+        }
+    } catch(err) {
+        res.status(400).send(err);
+    }
+}
+
+// export const logout = async(req: Request, res: Response) => {
+//     req.session.destroy(err => {
+//       if (err) {
+//         console.log(err);
+//         return res.status(500).send();
+//       } else {
+//         res.clearCookie('');
+//         res.redirect('/');
+//       }
+//     });
+// }

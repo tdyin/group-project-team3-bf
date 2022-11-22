e997a824f60c397852c9b7c8d6534492f2149ea6
JinyanL <jinyanli149@gmail.com>
2022-11-22 18:20:15 -0500

    Login page (#3)
    * login feature first implement
    
    * login feature first commit
    
    * Update package.json
    
    Co-authored-by: Jinyan Li <jinyanli@DESKTOP-G24AK6R.localdomain>
    Co-authored-by: tdyin <100390112+tdyin@users.noreply.github.com>

---
+++ client/src/components/login/Login.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import axios from 'axios';
+
+
+const theme = createTheme();
+
+const initialValues = {
+  username: '',
+  password: '',
+}
+
+export default function Login() {
+  const [values, setValues] = useState(initialValues);
+  const navigate = useNavigate();
+
+  const handleInputChange = (e:any) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(values)
+    axios.post('/login', values)
+      .then((res) => {
+        console.log(res)
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="username"
+              name="username"
+              autoComplete="username"
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              onChange={handleInputChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}

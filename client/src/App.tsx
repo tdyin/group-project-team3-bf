586edfef9c02d8145e9d64c97ca2cf78b9389401
JinyanL <jinyanli149@gmail.com>
2022-11-23 18:52:28 -0500

    Login page (#11)
    * login feature first implement
    
    * login feature first commit
    
    * Update package.json
    
    * Update package.json
    
    * move login file to pages
    
    * cookie still progress
    
    * cookie set up
    
    * validation adding
    
    * merge
    
    * conflicts resolve
    
    * Delete duplicate
    
    Co-authored-by: Jinyan Li <jinyanli@DESKTOP-G24AK6R.localdomain>
    Co-authored-by: tdyin <100390112+tdyin@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Register from './pages/Register'
 import Login from './pages/Login'
+import Register from './pages/Register'
 import Application from './pages/Application'
 
 function App() {

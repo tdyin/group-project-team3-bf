e5d3fbf585d62b928df0c1e0e1469230ea522e8d
John Nguyen <aznxmist@gmail.com>
2022-11-22 19:01:25 -0600

    Added register route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Test from './pages/Test'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/test' element={<Test />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>

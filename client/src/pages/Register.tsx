b0b0c8206881454a2ae6c128f9ce5ce0fda3b29e
John Nguyen <aznxmist@gmail.com>
2022-11-22 19:01:42 -0600

    Exported Register component

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,9 @@
+import RegistrationForm from "../components/registration/RegistrationForm"
+
+const Register: React.FC = () => {
+    return (
+        <RegistrationForm />
+    )
+}
+
+export default Register;

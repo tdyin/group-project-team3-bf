b1edd9101bb50d16279ac9a03797b55b82a87244
John Nguyen <aznxmist@gmail.com>
2022-11-25 10:06:24 -0600

    Changed routing for Employee links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,24 +10,31 @@ import Housing from './pages/hr/Housing'
 import Hiring from './pages/hr/Hiring'
 import Visa from './pages/hr/Visa'
 import Employees from './pages/hr/Employees'
+import Navbar, { DrawerHeader } from './components/nav/Navbar'
+import { CssBaseline, Box } from '@mui/material'
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/application' element={<Application />} />
-          <Route path='/emp/info' element={<MyInfo />} />
-          <Route path='/emp/visa' element={<MyVisa />} />
-          <Route path='/emp/housing' element={<MyHousing />} />
-          <Route path='/hr/employees' element={<Employees />} />
-          <Route path='/hr/visa' element={<Visa />} />
-          <Route path='/hr/hiring' element={<Hiring />} />
-          <Route path='/hr/housing' element={<Housing />} />
-        </Routes>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+            <Routes>
+              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/' element={<Home />} />
+              <Route path='/application' element={<Application />} />
+              <Route path="/emp" element={<Navbar />} >
+                <Route path='info' element={<MyInfo />} />
+                <Route path='visa' element={<MyVisa />} />
+                <Route path='housing' element={<MyHousing />} />
+              </Route>
+                <Route path='/hr/employees' element={<Employees />} />
+                <Route path='/hr/visa' element={<Visa />} />
+                <Route path='/hr/hiring' element={<Hiring />} />
+                <Route path='/hr/housing' element={<Housing />} />
+            </Routes>
+        </Box>
       </BrowserRouter>
     </>
   )

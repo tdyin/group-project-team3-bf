abaa137eeb67da36d35909c1fe240722bd431b84
tdyin <100390112+tdyin@users.noreply.github.com>
2022-11-25 09:48:20 -0800

    Nav (#16)
    * Add navbar and related pages
    
    * Removed Navbar from individual pages
    
    * Changed routing for Employee links
    
    * Added Outlet to show child components
    
    * Added marginRight styling
    
    * Modify routing and make up create default docs
    
    Co-authored-by: John Nguyen <aznxmist@gmail.com>

--- client/src/pages/employee/MyInfo.tsx
+++ client/src/pages/employee/MyInfo.tsx
@@ -1,15 +1,11 @@
-import { Box, CssBaseline } from '@mui/material'
-import Navbar, { DrawerHeader } from '../../components/nav/Navbar'
+import { Box } from '@mui/material'
+import { DrawerHeader } from '../../components/nav/Navbar'
 
 export default function MyInfo() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <Navbar />
-      <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-        Personal Information Page
-      </Box>
+    <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+      <DrawerHeader />
+      Personal Information Page
     </Box>
   )
 }

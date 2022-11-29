f1bca0a25e9d23b12121dea81470c7880cc67346
John Nguyen <aznxmist@gmail.com>
2022-11-29 11:27:48 -0600

    Added namespace types for process.env

---
+++ server/global.d.ts
@@ -0,0 +1,10 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        MONGO_URL: string;
+        PORT: number;
+        JWT_KEY: string;
+        AWS_ACCESS_KEY_ID: string;
+        AWS_SECRET_ACCESS_KEY: string
+    }
+}
+//File to allow process.env to be used

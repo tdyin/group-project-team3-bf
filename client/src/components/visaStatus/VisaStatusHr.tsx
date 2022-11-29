2a2646cffa86be9d955e4bcd19db3a451dafa93a
Jinyan Li <jinyanli@DESKTOP-G24AK6R.localdomain>
2022-11-29 17:19:52 -0500

    merge

--- client/src/components/visaStatus/VisaStatusHr.tsx
+++ client/src/components/visaStatus/VisaStatusHr.tsx
@@ -1,29 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// interface users {
-//   username: string
-// }
-
 const VisaStatusHr: React.FC = () => {
   const [users, setUsers] = useState([])
-
-  // function isOpt(arr: []) {
-  //   const result: [] = [];
-  //   for (let i = 0; i < arr.length; i++) {
-  //     if(arr[i]['legal']['visaTitle'] === 'F1(CPT/OPT)') {
-  //       result.push(arr[i])
-  //     }
-  //   }
-  //   return result;
-  // }
 
   useEffect(() => {
     axios.get('http://localhost:8080/emp/visa')
       .then((data) => {
-        // const userOpt = isOpt(data.data)
-        // setUsers(userOpt)
-        // console.log(userOpt)
         setUsers(data.data);
       })
       .catch((err) => {

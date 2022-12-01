97b7ecb002770cd83b473a6774570548c8cbdc6e
John Nguyen <aznxmist@gmail.com>
2022-12-01 11:28:53 -0600

    Added RegisterToken component to page

--- client/src/pages/hr/Hiring.tsx
+++ client/src/pages/hr/Hiring.tsx
@@ -1,11 +1,16 @@
+import { useEffect, useState } from 'react';
 import { Box } from '@mui/material'
 import { DrawerHeader } from '../../components/nav/Navbar'
+import RegisterToken from '../../components/hrmanagement/RegisterToken';
 
 export default function Hiring() {
+  //Map and add RegisterToken and Onboarding components, pass user info
   return (
     <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
       <DrawerHeader />
       HR Hiring Page
+
+      <RegisterToken/>
     </Box>
   )
 }

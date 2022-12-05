56590d48f1823033ffdc19d0bdd595acaf229df6
tdyin <tongda.yin@hotmail.com>
2022-12-04 19:09:51 -0500

    Resolve conflicts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,12 +3,15 @@ import userRoutes from './UserRoutes'
 import personalRoutes from './PersonalInfoRoutes'
 import s3Routes from './S3Routes';
 import applicationRoutes from './ApplicationsRoutes';
-import s3Routes from './S3Routes';
 import hrRoutes from './HRRoutes';
 
 const routes = Router();
 
 routes.use(userRoutes);
 routes.use(personalRoutes);
+routes.use(hrRoutes);
+
+routes.use(s3Routes)
+routes.use(applicationRoutes)
 
 export default routes;
